23d5cd2615ac4d371272d40dead84b0738c71627
CeYn97 <Ceyyyn97@gmail.com>
2025-06-09 20:59:35 +0300

    deploing-changes

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import styles from '../../styles/shared.module.css';
+import React from "react";
+import styles from "../../styles/Shared.module.css";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
 export const Button: React.FC<Props> = ({ label, ...props }) => (
-  <button className={styles.button} {...props}>{label}</button>
+  <button className={styles.button} {...props}>
+    {label}
+  </button>
 );
